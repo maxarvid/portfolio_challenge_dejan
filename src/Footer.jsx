26f9b0ce784793be7b9953d768e98ea97efe4553
maxarvid <max.arvid.anderson@gmail.com>
2022-02-11 11:01:41 +0100

    first commit

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-import { Container } from "semantic-ui-react"
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import { positions } from "@mui/system";
 
 const Footer = () => {
   return (
     <footer>
-      <p id="footer">Made with React {React.version}</p>
+      <Paper sx={{ position: "bottom" }} elevation={1}>
+        <Typography variant="h5" component="h3" id="footer">
+          Made with React {React.version}
+        </Typography>
+        <Typography component="p">@2018 All right reserved</Typography>
+      </Paper>
     </footer>
   );
 };
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,31 +1,48 @@
-import React from "react";
-import { Menu, Segment } from "semantic-ui-react";
-import { NavLink, Link } from "react-router-dom"
+import React, { useState } from "react";
+import { NavLink, Link } from "react-router-dom";
+import { Box, Tabs, Tab } from "@mui/material";
 
 const Header = () => {
+  const [value, setValue] = useState(0);
+
+  function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      "aria-controls": `simple-tabpanel-${index}`,
+    };
+  }
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
-    <Segment inverted>
-      <Menu inverted secondary>
-        <Menu.Item
-          id="header"
-          name="my portfolio"
-          as={Link}
-          to={{ pathname: "/" }}
-        />
-        <Menu.Item
-          id="about-tab"
-          name="about me"
-          as={NavLink}
-          to={{ pathname: "/about" }}
-        />
-        <Menu.Item
-          id="projects-tab"
-          name="my projects"
-          as={NavLink}
-          to={{ pathname: "/projects" }}
-        />
-      </Menu>
-    </Segment>
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+        >
+          <Tab
+            label="my portfolio"
+            {...a11yProps(0)}
+            as={Link}
+            to={{ pathname: "/" }}
+          />
+          <Tab
+            label="about me"
+            {...a11yProps(1)}
+            as={NavLink}
+            to={{ pathname: "/about" }}
+          />
+          <Tab
+            label="my projects"
+            {...a11yProps(2)}
+            as={NavLink}
+            to={{ pathname: "/projects" }}
+          />
+        </Tabs>
+      </Box>
+    </Box>
   );
 };
 
